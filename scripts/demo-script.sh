ec1763070a53eea30bbb65aa2a30e8a9695859ae
fccview <comm.campione@gmail.com>
2025-08-18 21:04:08 +0100

    clean up and final touches

---
+++ scripts/demo-script.sh
@@ -0,0 +1,6 @@
+# @id: demo-script
+# @title: Hi, this is a demo script
+# @description: This script logs a "hello world" to teach you how scripts work.
+
+#!/bin/bash
+echo 'Hello World' > hello.txt
