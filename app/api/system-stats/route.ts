888297c56a5cccada5888c69388d7b860c9ccb64
fccview <comm.campione@gmail.com>
2025-08-26 07:10:19 +0100

    final product for testers

--- app/api/system-stats/route.ts
+++ app/api/system-stats/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import * as si from 'systeminformation';
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
     try {
